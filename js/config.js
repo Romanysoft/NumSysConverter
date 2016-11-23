c2176f813db6c3f69a93d170929ea021a40c374c
iansun <lauer3912@gmail.com>
2016-11-23 14:44:54 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"NumSysConverter",
+        appID:"com.romanysoft.app.macos.NumSysConverter",
+        documentTitle:"NumSysConverter By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-87767476-2",
+        gitHome:"//github.com/Romanysoft/NumSysConverter",
+        reportIssueUrl:"//github.com/Romanysoft/NumSysConverter/issues",
+        changeLogUrl:"//github.com/Romanysoft/NumSysConverter/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/NumSysConverter/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
